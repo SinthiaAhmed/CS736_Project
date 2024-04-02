1659f8a6d94dcd20ff93865561939fbb1082702a
kimeya <kimeya.orin11@gmail.com>
2024-04-02 14:49:43 -0600

    my test

--- connect.js
+++ connect.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 // Replace with your actual connection string
-const uri = "mongodb+srv://sinthiaahmed904:p%+chvYZ4+tC_$2@<cluster-name>.mongodb.net/test?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://sinthiaahmed904:p%+chvYZ4+tC_$2@<cluster-name>.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://kimeyaorin2:pv63bjYCVfb6ct2m@cluster0.nyc5ntk.mongodb.net/?retryWrites=true&w=majority";
+// mongodb+srv://<username>:<password>@cluster0.nyc5ntk.mongodb.net/
+// const uri = "mongodb+srv://kimeyaorin2:YUKErFtcorwh9EMO@cluster0.nyc5ntk.mongodb.net/mydatabase?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB Atlas"))
